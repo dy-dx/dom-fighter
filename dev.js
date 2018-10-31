52d22fd09d01418945e71f60816d74b4d211ae61
Chris Kim <christopherjkim@gmail.com>
2018-10-31 18:16:22 -0400

    initialer commit

---
+++ dev.js
@@ -0,0 +1,23 @@
+const watch = require('tsc-watch/client');
+const notifier = require('node-notifier');
+const browserSync = require('browser-sync');
+
+const bs = browserSync.create();
+bs.init({
+  server: true,
+  files: [
+    'index.html',
+    'dist/**/*.js',
+  ],
+});
+
+watch.on('compile_errors', (lines) => {
+  notifier.notify({
+    group: 'dom-fighter',
+    title: 'dom-fighter',
+    message: lines || 'TypeScript compiler error',
+    timeout: 4,
+  });
+});
+
+watch.start();
